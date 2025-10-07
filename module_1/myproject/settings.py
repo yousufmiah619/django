25279fe3939fdebb0c9582fcdc684115ba211e18
yousufmiah619 <yousufmiah619@gmail.com>
2025-10-07 07:44:44 +0600

    django project,app,superuser create

--- module_1/myproject/settings.py
+++ module_1/myproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cc8&%6_+r7ov4i*^*8ri2oth-xar2#(_1u@#-3jx&3kufn-gc!'
+SECRET_KEY = 'django-insecure-ojd#6)bx__40uvouxm1-4+ed%l3!afsc&9kr61*-ci&^#xkeq0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
